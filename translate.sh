1eef17e9cbc524ba9039ed112fa556d00f4cbd37
s18014 <s18014@std.it-college.ac.jp>
2018-08-29 16:08:26 +0900

    同じディレクトリに置けば機能するように変更

--- translate.sh
+++ translate.sh
@@ -4,6 +4,6 @@ base=$(cd $(dirname $0); pwd)
 
 zenity --notification --text=検索中\\n$(xsel) --timeout=1
 
-python3 ~/bin/translate/rewrite.py
+python3 translate/rewrite.py
 
 zenity --title=辞書 --width=400 --height=300 --text-info --html --filename=${base}/translate/tmp.html --timeout=30
